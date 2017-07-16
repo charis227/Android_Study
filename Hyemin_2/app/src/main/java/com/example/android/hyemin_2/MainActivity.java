35d87faa98936eb83a9dbf191197ea16e33c34ed
Hyemin Kim <1117hyemin@gmail.com>
2017-07-16 18:35:12 +0900

    Add function for quiz result
    save quiz result string and total score.

--- Hyemin_2/app/src/main/java/com/example/android/hyemin_2/MainActivity.java
+++ Hyemin_2/app/src/main/java/com/example/android/hyemin_2/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.android.hyemin_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    int score = 0;
+    String result = "";
+    RadioButton quiz[] = new RadioButton[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitQuiz(){
+
+    }
+
+    private void getResult(){
+        quiz[0] = (RadioButton) findViewById(R.id.correct_answer1);
+        quiz[1] = (RadioButton) findViewById(R.id.correct_answer2);
+        quiz[2] = (RadioButton) findViewById(R.id.correct_answer3);
+
+        result += "Your Result: \n";
+
+        for(int i=0; i<quiz.length; i++) {
+            if(quiz[i].isChecked()) {
+                result += "Quiz"+(i+1)+": O\n";
+                score += 20;
+            }
+            else {
+                result += "Quiz"+(i+1)+": X\n";
+            }
+        }
+
+        result += "Total score: "+score+"/100\n";
     }
 }
