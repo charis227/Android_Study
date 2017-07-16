f0b4645cc584b7d8b5b41757825f44e1f1c685e8
Hyemin Kim <1117hyemin@gmail.com>
2017-07-16 19:55:29 +0900

    Add remark in java code
    remark: purpose of functions and meaning of paramaters

--- Hyemin_2/app/src/main/java/com/example/android/hyemin_2/MainActivity.java
+++ Hyemin_2/app/src/main/java/com/example/android/hyemin_2/MainActivity.java
@@ -9,7 +9,10 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * @param score         quiz score
+     * @param quiz[]        each quiz's correct answer RadioButton
+     */
     int score = 0;
     RadioButton quiz[] = new RadioButton[3];
 
@@ -19,24 +22,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * This method submit result into mail application.
+     *
+     * @param view          submit Button
+     */
     public void submitQuiz(View view){
+        /**
+         * @param name          user name
+         * @param result        result string to show correct or not in each quiz and total score
+         */
         String name = getName();
         String result = getResult();
 
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:")); // only email apps should handle this
-        intent.putExtra(Intent.EXTRA_SUBJECT, name+"'s Quiz Result");
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.result)+name);
         intent.putExtra(Intent.EXTRA_TEXT, result);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
     }
 
+    /**
+     * This method get user name from edit text.
+     */
     private String getName(){
         EditText name = (EditText) findViewById(R.id.name_edit_txt);
         return name.getText().toString();
     }
 
+    /**
+     * This method get each result in quiz and calculate total score.
+     * @return          all result string in the quiz page.
+     */
     private String getResult(){
         String result="";
 
@@ -44,19 +63,19 @@ public class MainActivity extends AppCompatActivity {
         quiz[1] = (RadioButton) findViewById(R.id.correct_answer2);
         quiz[2] = (RadioButton) findViewById(R.id.correct_answer3);
 
-        result += "Your Result: \n";
+        result += getString(R.string.your_result)+"\n\n";
 
         for(int i=0; i<quiz.length; i++) {
             if(quiz[i].isChecked()) {
-                result += "Quiz"+(i+1)+": O\n";
+                result += getString(R.string.quiz)+(i+1)+": O\n";
                 score += 1;
             }
             else {
-                result += "Quiz"+(i+1)+": X\n";
+                result += getString(R.string.quiz)+(i+1)+": X\n";
             }
         }
 
-        result += "Total score: "+score+"/3\n";
+        result += getString(R.string.total_score)+score+"/3\n";
 
         return result;
     }
